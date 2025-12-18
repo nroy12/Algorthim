6faa7190a797ff4d53179767dcef176f390d3d7d
nroy12 <nitinroy39613@gamil.com>
2025-12-18 08:28:44 +0530

    added optimisize and brute methods

--- kadane.cpp
+++ kadane.cpp
@@ -20,7 +20,7 @@ public:
 
     // methods of class:
     int brute_force();
-
+    int optimize();
     // This is class functions
     void getter()
     {
@@ -49,20 +49,44 @@ int kadane ::brute_force()
             }
         }
     }
-
     return maxsum;
 }
 
+
+int kadane:: optimize(){
+
+    int sum=0;
+    int maxi = INT_MIN;
+    for(int i=0;i<10;i++){
+      sum=sum+arr[i];
+      maxi = max(sum,maxi);
+      if(sum<0){
+        sum = 0;
+      }
+    }
+ 
+    return maxi;
+
+
+}
 int main()
 {
     int myarr[10];
+    int mysecondarr[10];
     for(int i = 0;i<10;i++){
         cout<<i+1<<" th element : ";
         cin>> myarr[i];
+        mysecondarr[i] = myarr[i];
     }
+    
     kadane k1test(myarr);
     int maxvalue = k1test.brute_force();
     cout<<"Maximum subarray value is : " << maxvalue<<"\n";
+    
+    kadane k2test(mysecondarr);
+    maxvalue = k2test.optimize();
+    cout<<"Maximum subarray value is : " << maxvalue<<"\n";
+
 
     return 0;
 }
